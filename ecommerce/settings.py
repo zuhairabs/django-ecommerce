d6c06e1c01495610751888da3f638ecc7c5d38ea
Zuhair <zuhairabs@gmail.com>
2020-12-10 10:26:17 +0530

    DB UPDATE

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -92,8 +92,12 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd9pfmo8i7v8mg0',
+        'USER': 'hipvprdoxliddp',
+        'PASSWORD': '8e27d81b760bbe1680eee96f3cde123d0624dc0def5fb0199662eac4fe87d0b4',
+        'HOST': 'ec2-18-206-103-49.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
